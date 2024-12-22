1e77c6385d852446e5bbd1dec848af8c823f4144
Paul Donovan <paul.m.donovan@outlook.com>
2024-12-22 14:02:57 +1100

    added changes

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,10 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Title from './Title';
 import { ShopContext } from '../context/ShopContext'
 
 const LatestCollection = () => {
 
     const { products } = useContext(ShopContext);
+    const [latestProducts, setLatestProducts] = useState([]);
+
+    useEffect(() => {
+
+        setLatestProducts(products.slice(0, 10));
+
+    }, [])
 
 
     return (
